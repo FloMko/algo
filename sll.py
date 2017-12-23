2f8638d63cdb5ec509186fe108298602fd536e82
flomko <flomko.ru@gmail.com>
2017-12-23 13:50:22 +0300

    add iteration over list

--- sll.py
+++ sll.py
@@ -1,13 +1,22 @@
-class SinglyLinkedList:
-    def __init__(self):
-        self.tail = None
-    def append(self, data):
-        node = Node(data)
-        if self.tail==None:
-            self.tail = node
-        else:
-            current = self.tail
-        while current.next:
-            current = current.next
-            current.next = node
-
+class SinglyLinkedList:    
+     def __init__(self):    
+         self.tail = None         
+         self.head = None         
+         self.size = 0            
+     def append(self, data):      
+         self.size +=1       
+         node = Node(data)        
+         if self.head:       
+             self.head.next = node
+             self.head = node
+         else:               
+             self.tail = node  
+             self.head = node      
+     def size(self):               
+         return self.size          
+     def iter(self):               
+         current = self.tail   
+         while current:            
+             val = current.data    
+             current = current.next    
+             yield val                       
