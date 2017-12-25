dedd7e76d8360ac2c954b22fbb2e0d40aec8c671
flomko <flomko.ru@gmail.com>
2017-12-25 19:04:47 +0300

    init double linked list

---
+++ dll.py
@@ -0,0 +1,11 @@
+class Node(object):
+    def __init__(self, data=None, next=None, prev=None)
+        self.data = data
+        self.next = next
+        self.prev = prev
+
+class DoublyLinkedList(object):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.count = 0
