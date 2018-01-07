9d2ae7b1676183a2787b6a87e2140eb1ab76f061
flomko <flomko.ru@gmail.com>
2018-01-07 14:39:12 +0300

    init add track method, dont work

--- tracks.py
+++ tracks.py
@@ -1,5 +1,14 @@
 from random import randint
+import time
+from queue import Node, NodeQueue
+
 class Track:
     def __init__(self, title=None):
         self.title = title
         self.length = randint(5,10)
+
+class MediaPlayerQueue(NodeQueue):
+    def __init__(self):
+        super(MediaPlayerQueue, self).__init__()
+    def add_track(self, track):
+        self.enqueue(track)
