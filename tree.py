28f25140c4384c2bcb2893195375d82a8e9aea2e
flomko <flomko.ru@gmail.com>
2018-01-11 23:02:51 +0300

    bst insert method add

--- tree.py
+++ tree.py
@@ -18,4 +18,24 @@ class Bst:
         while current.right_child:
             current = current.right_child
         return current
+    def insert(self, data):
+        node = Node(data)
+        if self.root_node is None:
+            self.root_node = node
+        else:
+            current = self.root_node
+            parent = None
+            while True:
+                parent = current
+                if node.data < current.data:
+                    current = current.left_child
+                    if current is None:
+                        parent.left_child = node
+                        return
+                else:
+                    current = current.right_child
+                    if current is None:
+                        parent.right_child = node
+                        return
+
 
