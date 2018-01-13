d6898299a56a4c9690fee92da0a7f4be2bdaeae9
flomko <flomko.ru@gmail.com>
2018-01-13 17:52:03 +0300

    add search for parent

--- tree.py
+++ tree.py
@@ -37,5 +37,19 @@ class Bst:
                     if current is None:
                         parent.right_child = node
                         return
-
+    def get_node_with_parent(self, data):
+        parent = None
+        current = self.root_node
+        if current is None:
+            return (parent, None)
+        while True:
+            if current.data == data:
+                return (parent, current)
+            elif current.data > data:
+                parent = current
+                current = current.left_child
+            else:
+                parent = current
+                current = current.right_child
+                return (parent, current)
 
