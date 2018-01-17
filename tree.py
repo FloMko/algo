672eeaa245445fd5e4b2640675edec4f71031ffe
flomko <flomko.ru@gmail.com>
2018-01-17 22:30:26 +0300

    add inorder traversal

--- tree.py
+++ tree.py
@@ -107,3 +107,10 @@ class Bst:
                 current = current.left_child
             else:
                 current = current.right_child
+    def inorder(self, root_node):
+        current = root_node
+        if current is None:
+            return
+        self.inorder(current.left_child)
+        print(current.data)
+        self.inorder(current.right_child)
