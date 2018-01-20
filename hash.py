91e13d6691267359b7770a2919fb0bd19de4aa1a
flomko <flomko.ru@gmail.com>
2018-01-20 14:44:37 +0300

    add hash table constructor

--- hash.py
+++ hash.py
@@ -5,3 +5,14 @@ def myhash(s):
         hv += mult * ord(ch)
         mult += 1
     return hv
+
+class HashItem:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+class HashTable:
+    def __init__(self):
+        self.size = 256
+        self.slots = [None for i in range(self.size)]
+        self.count = 0
