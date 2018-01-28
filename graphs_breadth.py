98f2b25590fe291a968ce78f2e904818679771e3
flomko <flomko.ru@gmail.com>
2018-01-28 14:33:44 +0300

    fix breadth

--- graphs_breadth.py
+++ graphs_breadth.py
@@ -15,7 +15,7 @@ def prepare():
 def breadth_first_search(graph, root):
     visited_vertices = list()
     graph_queue = deque([root])
-    visited_vertices.apend(root)
+    visited_vertices.append(root)
     node = root
     while len(graph_queue) >0:
         node = graph_queue.popleft()
@@ -23,33 +23,14 @@ def breadth_first_search(graph, root):
         remaining_elements = set(adj_nodes).difference(set(visited_vertices))
         if len(remaining_elements) > 0:
             for elem in sorted(remaining_elements):
-                visited_verticies.append(elem)
+                visited_vertices.append(elem)
                 graph_queue.append(elem)
     return visited_vertices
 
-def depth_first_search(graph, root):
-    visited_vertices = list()
-    graph_stack = list()
-    graph_stack.append(root)
-    node = root
-    while len(graph_stack) > 0:
-        if node not in visited_vertices:
-            visited_vertices.append(node)
-            adj_nodes = graph[node]
-            if set(adj_nodes).issubset(set(visited_vertices)):
-                graph_stack.pop()
-            if len(graph_stack) > 0:
-                node = graph_stack[-1]
-            continue
-        else:
-            remaining_elements = set(adj_nodes).difference(set(visited_vertices))
-            first_adj_node = sorted(remaining_elements)[0]
-            graph_stack.append(first_adj_node)
-            node = first_adj_node
-    return visited_vertices
-
 def main():
-    graphs = prepare()
+    graph = prepare()
+    visit = breadth_first_search(graph,'A')
+    print(visit)
 
 if __name__ == '__main__':
     main()
