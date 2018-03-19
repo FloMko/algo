245db1adb69f9f0d06c12eae1798d2599c997af9
flomko <flomko.ru@gmail.com>
2018-03-19 21:38:39 +0300

    adding insertionSort

--- sort.py
+++ sort.py
@@ -7,4 +7,14 @@ def bubbleSort(unordered_list):
                 unordered_list[j] = unordered_list[j+1]
                 unordered_list[j+1] = temp
     return unordered_list
-   
+
+
+def insertionSort(unsorted_list):
+    for index in range(1, len(unsorted_list)):
+        search_index = index
+        insert_value = unsorted_list[index]
+        while search_index > 0 and unsorted_list[search_index -1] > insert_value:
+            unsorted_list[search_index] = unsorted_list[search_index-1]
+            search_index -= 1
+            unsorted_list[search_index] = insert_value
+    return unsorted_list
