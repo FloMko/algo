6332a3fa721e6ac54c390dd97200e38809d6b217
flomko <flomko.ru@gmail.com>
2018-04-04 21:54:09 +0300

    add dynamic implement fibbonachi.. not worked

---
+++ fib_dyn.py
@@ -0,0 +1,15 @@
+
+def fib(n):
+    if n <= 2:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+
+def dyna_fib(n, lookup):
+    if n <= 2:
+        lookup[n] = 1
+    if lookup[n] is None:
+        lookup[n] = dyna_fib(n-1, lookup) + dyna_fib(n-2, lookup)
+        return lookup[n]
+
