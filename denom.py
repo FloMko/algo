1053a9d949ae165a27dea66b609f97d3015f3698
Maksimillian <maksim.ermolenko@tra.ai>
2018-04-05 19:44:34 +0300

    start solve denom prob as greedy algo

---
+++ denom.py
@@ -0,0 +1,12 @@
+def basic_small_change(denom, total_amount):
+    sorted_denominations = sorted(denom, reverse=True)
+
+    number_of_denoms = []
+
+    for i in sorted_denominations:
+        div = total_amount // i
+        total_amount = total_amount % i
+        if div > 0:
+            number_of_denoms.append((i, div))
+
+    return number_of_denoms
